035f82bb00f766578ac2daba90e397e0d7b1338b
Stuart Walton <stuart@stickypiston.co>
2023-12-06 05:30:55 +0000

    Redo Day 03 - Part 1 Use sliding window to convolve Processes in 1 pass of the input!

--- 03/solution.py
+++ 03/solution.py
@@ -1,7 +1,8 @@
+from collections import deque
 from io import TextIOWrapper
-from operator import and_, or_
+from itertools import chain, islice
 from os import path
-from typing import Callable, Sequence
+from typing import Callable, Iterator, Sequence, Tuple
 
 FOLDER = path.dirname(__file__)
 INPUT_EXAMPLE = path.join(FOLDER, "input_example.txt")
@@ -13,28 +14,30 @@ def iter_line(file_object: TextIOWrapper):
         yield line.strip()
 
 
-def conv_3[T, U](seq: Sequence[T], fill: T, func: Callable[[T, T], U]) -> Sequence[U]:
-    gen = (x for x in seq)
-    first = fill
-    try:
-        second = next(gen)
-    except StopIteration:
-        second = fill
-    try:
-        third = next(gen)
-    except StopIteration:
-        third = fill
-    yield func(second, third)
-    for incoming in gen:
-        first = second
-        second = third
-        third = incoming
-        yield func(func(first, second), third)
-    yield func(func(fill, second), third)
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def sliding_window(iterable, n):
+    it = iter(iterable)
+    window = deque(islice(it, n - 1), maxlen=n)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
 
 
-def linear_or[T](a: Sequence[T], b: Sequence[T]) -> Sequence[T]:
-    return (or_(*pair) for pair in zip(a, b))
+def conv_3_3[T, U](
+    seq: Sequence[T],
+    fill: T,
+    func: Callable[[Tuple[Tuple[T, T, T], Tuple[T, T, T], Tuple[T, T, T]]], U],
+) -> Iterator[U]:
+    fill_ = fill[0:1]
+    for first, second, third in sliding_window(chain([fill], seq, [fill]), 3):
+        yield (
+            func(x)
+            for x in zip(
+                sliding_window(chain(fill_, first, fill_), 3),
+                sliding_window(chain(fill_, second, fill_), 3),
+                sliding_window(chain(fill_, third, fill_), 3),
+            )
+        )
 
 
 def sum_line_nums(line, num_row, comb_row) -> int:
@@ -56,37 +59,47 @@ def sum_line_nums(line, num_row, comb_row) -> int:
     return acc
 
 
+def part_1_conv(
+    lens: Tuple[Tuple[str, str, str], Tuple[str, str, str], Tuple[str, str, str]]
+) -> Tuple[str, int]:
+    """Takes a 3x3 tuple of characters and returns a tuple of the center character
+    with a mask of:
+        0   -   Not a digit
+        1   -   Digit but no symbols
+        2   -   Digit with a symbol"""
+    char = lens[1][1]
+    is_num = 1 if char.isnumeric() else 0
+    return (
+        char,
+        is_num * 2
+        if any(any((not x.isnumeric() and x != ".") for x in y) for y in lens)
+        else is_num,
+    )
+
+
+def nums_from_line_mask(line_mask: Sequence[Tuple[str, int]]) -> Iterator[int]:
+    acc = ""
+    sym = False
+    for char, mask in line_mask:
+        if mask == 0:
+            if acc and sym:
+                yield int(acc)
+            acc = ""
+            sym = False
+        else:
+            acc += char
+        sym |= mask > 1
+    if acc and sym:
+        yield int(acc)
+
+
 def part_1(input_file: TextIOWrapper) -> int:
     input_file.seek(0)
     stride = len(input_file.readline().strip())
-
-    input_file.seek(0)
-    number_mask = [[c.isnumeric() for c in line] for line in iter_line(input_file)]
-
-    input_file.seek(0)
-    symbol_mask = list(
-        conv_3(
-            (
-                list(conv_3((not c.isnumeric() and c != "." for c in line), False, or_))
-                for line in iter_line(input_file)
-            ),
-            [False for _ in range(stride)],
-            linear_or,
-        )
-    )
-
-    combined_mask = [
-        [and_(*pair) for pair in zip(*row_pair)]
-        for row_pair in zip(number_mask, symbol_mask)
-    ]
-
     input_file.seek(0)
     return sum(
-        sum_line_nums(line, num_row, comb_row)
-        for line, num_row, comb_row in zip(
-            iter_line(input_file), number_mask, combined_mask
-        )
-        if any(comb_row)
+        sum(nums_from_line_mask(line_mask))
+        for line_mask in conv_3_3(iter_line(input_file), "." * stride, part_1_conv)
     )
 
 
